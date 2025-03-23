fe09150f7c2fcd6e397df21310206a234983fa1f
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-03-23 12:45:53 +0530

    Added Chakra-ui to the application

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,16 @@
 // src/app/layout.js
 "use client";
+import { ChakraProvider } from '@chakra-ui/react'
 import ClientProviders from "./ClientProviders";
 import { NotesProvider } from "../context/NotesContext";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <ClientProviders>
-          <NotesProvider>{children}</NotesProvider>
+          <ChakraProvider><NotesProvider>{children}</NotesProvider></ChakraProvider>
         </ClientProviders>
       </body>
     </html>
